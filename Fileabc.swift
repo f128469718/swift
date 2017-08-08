b15262a4f2d75b32be316ee13f5b744e3bba7b68
f128469718 <cake3v2e69>
2017-08-08 16:28:40 +0800

    Fileabc
    Fileabc

---
+++ Fileabc.swift
@@ -0,0 +1,18 @@
+//
+//  CreateScene.swift
+//  Riddle Me There
+//
+//  Created by admin on 2017/7/31.
+//  Copyright © 2017年 admin. All rights reserved.
+//
+import SpriteKit
+import GameplayKit
+import UIKit
+
+
+class Fileabc: SKScene {
+    var sView : SKView?
+    
+
+}
+
--- Riddle Me There/File123.swift
+++
@@ -1,9 +0,0 @@
-//
-//  File123.swift
-//  Riddle Me There
-//
-//  Created by admin on 2017/8/8.
-//  Copyright © 2017年 admin. All rights reserved.
-//
-
-import Foundation
