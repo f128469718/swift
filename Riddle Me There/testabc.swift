9acb67af08565cd74357feaab950a55b43dfa27d
kevin999123 <karta0832590@gmail.com>
2017-08-08 16:34:08 +0800

    test Scene

---
+++ Riddle Me There/testabc.swift
@@ -0,0 +1,20 @@
+//
+//  CreateScene.swift
+//  Riddle Me There
+//
+//  Created by admin on 2017/7/31.
+//  Copyright © 2017年 admin. All rights reserved.
+//
+import SpriteKit
+import GameplayKit
+import UIKit
+
+
+class testabc: SKScene {
+    var sView : SKView?
+    
+    
+}
+
+
+
