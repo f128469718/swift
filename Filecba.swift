bce5dd967258a22845c47b5422b4a42343fe47e9
f128469718 <cake3v2e69>
2017-08-08 16:31:31 +0800

    Filecba
    Filecba

---
+++ Filecba.swift
@@ -0,0 +1,19 @@
+//
+//  CreateScene.swift
+//  Riddle Me There
+//
+//  Created by admin on 2017/7/31.
+//  Copyright © 2017年 admin. All rights reserved.
+//
+import SpriteKit
+import GameplayKit
+import UIKit
+
+
+class Filecba: SKScene {
+    var sView : SKView?
+    
+    
+}
+
+
