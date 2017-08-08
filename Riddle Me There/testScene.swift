35d3d1b4ef4c7b886d1f6337c3a1b1b3909e9de6
kevin999123 <karta0832590@gmail.com>
2017-08-08 16:04:30 +0800

    test
    test

---
+++ Riddle Me There/testScene.swift
@@ -0,0 +1,22 @@
+//
+//  testScene.swift
+//  Riddle Me There
+//
+//  Created by admin on 2017/8/8.
+//  Copyright © 2017年 admin. All rights reserved.
+//
+
+import Foundation
+import SpriteKit
+
+class testScene: SKScene {
+    var sView : SKView?
+    
+    
+    override func didMove(to view: SKView) {
+        sView = self.view
+       
+        
+    }
+   
+}
