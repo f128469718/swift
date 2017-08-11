fa54d63ee589e7d8dbd8ba08bae5ac6b39ba003b
f128469718 <cake3v2e69>
2017-08-11 12:21:51 +0800

    ScoresScene

--- Riddle Me There/ScoresScene.swift
+++ Riddle Me There/ScoresScene.swift
@@ -24,17 +24,11 @@ class ScoresScene: SKScene {
     
     override func didMove(to view: SKView) {
         sView = self.view
-        let background = SKSpriteNode(imageNamed: "background-9")
-        background.position = CGPoint(x: size.width/2, y: size.height/2)
         
-        let view1 = SKSpriteNode(imageNamed: "VIEW")
-        view1.position = CGPoint(x: size.width/2, y: size.height/2)
-        
-        let reel = SKSpriteNode(imageNamed: "reel-1")
-        reel.position = CGPoint(x: size.width/2, y: size.height/2)
-        
-        let friends = SKSpriteNode(imageNamed: "friends")
-        friends.position = CGPoint(x: size.width/2, y: size.height/2)
+        let background = pos.imageclass(image: "background-9", x: size.width/2, y: size.height/2,z:-1)
+        let view1 = pos.imageclass(image: "VIEW", x: size.width/5.8, y: size.height/2.5,z:0)
+        let reel = pos.imageclass(image: "reel-1", x: size.width/1.05, y: size.height/2.5,z:0)
+        let friends = pos.imageclass(image: "friends", x: size.width/2, y: size.height/2,z:1)
         
         returnPress = UIButton(frame:CGRect(x: 20, y: 20, width: 120, height: 40))
         returnPress.setBackgroundImage(UIImage(named: "returnPress"), for: UIControlState.normal)
@@ -46,7 +40,7 @@ class ScoresScene: SKScene {
         
         returnPress.translatesAutoresizingMaskIntoConstraints = false
 
-        
+        pos.autoPosition(item1: returnPress, item2: self.view!, topValue: view.bounds.height * 0, bottomValue: -(view.bounds.height * 1), leftValue: view.bounds.width * 0, rightValue: -(view.bounds.width * 1), widthValue: 0.14, heightValue: 0.19)
         
     }
     @objc func clickButton(sender:UIButton){
